6903561bc6337ffe8f6776de7582a64adff9c6c3
Erik Moeiniche Kiebe (63037075) <erik.kiebe@dentsu.com>
2025-10-06 23:42:03 +0200

    refactor to use unstable

--- flake.nix
+++ flake.nix
@@ -2,10 +2,8 @@
   description = "Eriks nix-darwin system flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
 
     # nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
@@ -17,12 +15,12 @@
     # home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, nix-darwin, ... }:
+  outputs = { self, nixpkgs, nix-darwin, ... }:
   let
     pkgs = nixpkgs.legacyPackages.aarch64-darwin;
   in
   let
-    configuration = { pkgs, ... }: {
+    configuration = { pkgs, lib, ... }: {
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
 
@@ -69,38 +67,42 @@
 
       fonts.packages = with pkgs; [
           # developers fonts
-          dina-font
-          fira
-          fira-code
-          fira-code-symbols
-          jetbrains-mono
-          julia-mono
-
-          (nerdfonts.override { fonts = [
-            "JetBrainsMono"
-            "FiraCode"
-            "Iosevka"
-            "Hack"
-            "Meslo"
-            # "TerminessTTF"
-            "Inconsolata"
-            ]; }
-          )
-        #   nerd-fonts.jetbrains-mono
-        #   nerd-fonts.fira-code
-        #   nerd-fonts.iosevka
-        #   nerd-fonts.hack
-        #   nerd-fonts.meslo-lg
-        #   nerd-fonts.terminess-ttf
-        #   nerd-fonts.inconsolata
+          nerd-fonts.jetbrains-mono
+          nerd-fonts.fira-code
+          nerd-fonts.iosevka
+          nerd-fonts.hack
+          nerd-fonts.meslo-lg
+          nerd-fonts.inconsolata
       ];
 
-      # Unlocking sudo via fingerprint
-    #   security.pam.services.sudo_local.touchIdAuth = true;
-      security.pam.enableSudoTouchIdAuth = true;
+      # Don't manage PAM files (managed by IT)
+      system.activationScripts.pam.text = lib.mkForce "";
 
       # System defaults
       system.defaults = {
+        NSGlobalDomain = {
+          # Keyboard responsiveness
+          InitialKeyRepeat = 10;
+          KeyRepeat = 1;
+          ApplePressAndHoldEnabled = false;
+
+          # Disable smart text substitutions
+          NSAutomaticCapitalizationEnabled = false;
+          NSAutomaticDashSubstitutionEnabled = false;
+          NSAutomaticPeriodSubstitutionEnabled = false;
+          NSAutomaticQuoteSubstitutionEnabled = false;
+          NSAutomaticSpellingCorrectionEnabled = false;
+
+          # Better defaults
+          AppleShowAllExtensions = true;
+          NSDocumentSaveNewDocumentsToCloud = false;
+          NSNavPanelExpandedStateForSaveMode = true;
+          PMPrintingExpandedStateForPrint = true;
+
+          # Faster UI
+          NSAutomaticWindowAnimationsEnabled = false;
+        };
+
         dock = {
             autohide = true;
             magnification = false;
@@ -144,6 +146,21 @@
         experimental-features = ["nix-command" "flakes"];
         warn-dirty = false;
         # auto-optimise-store = true;
+
+        # Binary caches for faster downloads
+        substituters = [
+          "https://cache.nixos.org"  # Default cache
+          "https://nix-community.cachix.org"
+        ];
+
+        trusted-public-keys = [
+          "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        ];
+
+        # Performance optimizations
+        http-connections = 128;
+        max-jobs = "auto";
       };
 
       nix.optimise.automatic = true;
@@ -156,7 +173,10 @@
 
       # Used for backwards compatibility, please read the changelog before changing.
       # $ darwin-rebuild changelog
-      system.stateVersion = 1;
+      system.stateVersion = 5;
+
+      # Primary user for user-specific settings
+      system.primaryUser = "Erik.kiebe";
 
       # The platform the configuration will be used on.
       nixpkgs.hostPlatform = "aarch64-darwin";
